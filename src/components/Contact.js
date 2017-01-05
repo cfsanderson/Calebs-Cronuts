71198a9e420ce9e68293e4289981ec93c15275b2
Caleb Frost Sanderson <calebsanderson@gmail.com>
2017-01-05 08:46:25 -0500

    Added my avatar and bio using the GitHub API to the Contact page. Also started adding social buttons but still need styling ala my Portfolio page. git push cd ..

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,46 @@ import React, { Component } from 'react'
 
 class Contact extends Component {
 
+  constructor () {
+    super()
+    this.state = {
+      name: '',
+      avatar_url: '',
+      bio: '',
+      location: '',
+      blog: []
+    }
+  }
+
+  componentDidMount () {
+    const GitHubURL = 'https://api.github.com/users/cfsanderson'
+    window.fetch(GitHubURL).then((response) => {
+      return response.json()
+    }).then((data) => {
+      this.setState({
+        name: data.name,
+        avatar_url: data.avatar_url,
+        bio: data.bio,
+        location: data.location
+      })
+    })
+  }
+
   render () {
     return (
       <div>
         <h2>Contact Us</h2>
+        <div className='bio'>
+          <p>{this.state.bio}</p>
+          <img className='avatar_url' src={this.state.avatar_url} />
+        </div>
+        <ul className='socials'>
+          <li><a href='https://twitter.com/cfsanderson' target='_blank'><i className='fa fa-twitter' aria-hidden='true' /></a></li>
+          <li><a href='https://www.facebook.com/caleb.f.sanderson' target='_blank'><i className='fa fa-facebook' aria-hidden='true' /></a></li>
+          <li><a href='https://www.instagram.com/cfsanderson/' target='_blank'><i className='fa fa-instagram' aria-hidden='true' /></a></li>
+          <li><a href='https://www.linkedin.com/in/calebsanderson' target='_blank'><i className='fa fa-linkedin' aria-hidden='true' /></a></li>
+          <li><a href='https://github.com/cfsanderson' target='_blank'><i className='fa fa-github' aria-hidden='true' /></a></li>
+        </ul>
       </div>
     )
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,12 @@ class Home extends Component {
 
   render () {
     return (
-      <div>
-        <h1 />
-        <p>Welcome to The Donut Experiment®, where every donut is your creation! We start with a delicious, fresh-made cake donut and then you pick from our huge selection of icings and toppings. Yours can be as simple as plain or glazed or as crazy as key lime, maple bacon or Sriracha. Stop by today and begin your own experiment.</p>
+      <div className='home'>
+        <div className='cronut01-image'>
+          <p>Cronut image</p>
+        </div>
+        <h1>The Cronut</h1>
+        <p>A <a href='https://en.wikipedia.org/wiki/Cronut'>cronut</a> is a croissant-doughnut pastry invented by New York City pastry chef <a href='https://en.wikipedia.org/wiki/Dominique_Ansel'>Dominique Ansel</a> and trademarked by Dominique Ansel Bakery. The pastry resembles a doughnut and is made from croissant-like dough which is filled with flavored cream and fried in oil. The flavor of the pastry differs every month.</p>
       </div>
     )
   }
